a00ea2bfab2011a36415fa9d4a3a9a23fc0498b2
Brendan Armstrong <Brendan.Armstrong@mail.com>
2021-12-01 08:47:52 -0500

    Challange one completed

---
+++ Dec-1/part2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+
+  FILE *fd = fopen("/home/brendan/S/AoC/AoC-2021/Dec-1/input.txt", "r");
+  char buff[100];
+  int array[2000];
+  int index = 0;
+  int count = 0;
+  while (fgets(buff, 100, fd) != NULL) {
+    array[index++] = atoi(buff);
+  }
+  for (int i = 3; i < 2000; ++i) {
+    if (array[i] - array[i - 3] > 0) {
+      count += 1;
+    }
+  }
+  printf("%d\n", count);
+  fclose(fd);
+  return 0;
+}
