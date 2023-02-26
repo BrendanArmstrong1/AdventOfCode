85e4553f038783f3cd035761b2d054188b1de5b3
Brendan Armstrong <brendanarmstrong@proton.me>
2023-02-26 12:59:08 -0500

    day2 of AoC2022 completed

--- 2022/dec-2/src/main.rs
+++ 2022/dec-2/src/main.rs
@@ -1,6 +1,103 @@
-use std::fs;
+use std::{
+    fs,
+    io::{BufRead, BufReader},
+};
 
 fn main() {
-    let file = fs::read_to_string("input.txt").unwrap();
-    println!("{}", file);
+    let file = fs::File::open("input.txt").unwrap();
+    part1(file);
+    let file = fs::File::open("input.txt").unwrap();
+    part2(file);
+}
+
+fn part1(f: fs::File) {
+    let reader = BufReader::new(f);
+    let win = 6;
+    let lose = 0;
+    let draw = 3;
+
+    let score = reader.lines().map(Result::unwrap).fold(0, |acc, str| {
+        acc + {
+            match str.split_once(" ") {
+                Some((opp, me)) => match opp {
+                    // rock
+                    "A" => match me {
+                        "X" => 1 + draw,
+                        "Y" => 2 + win,
+                        "Z" => 3 + lose,
+                        _ => 0,
+                    },
+                    // paper
+                    "B" => match me {
+                        "X" => 1 + lose,
+                        "Y" => 2 + draw,
+                        "Z" => 3 + win,
+                        _ => 0,
+                    },
+                    // scissors
+                    "C" => match me {
+                        "X" => 1 + win,
+                        "Y" => 2 + lose,
+                        "Z" => 3 + draw,
+                        _ => 0,
+                    },
+                    _ => 0,
+                },
+                None => 0,
+            }
+        }
+    });
+    println!("part1: {}", score);
+}
+
+fn part2(f: fs::File) {
+    let reader = BufReader::new(f);
+    let win = 6;
+    let lose = 0;
+    let draw = 3;
+    let rock = 1;
+    let paper = 2;
+    let scis = 3;
+
+    let score = reader.lines().map(Result::unwrap).fold(0, |acc, str| {
+        acc + {
+            match str.split_once(" ") {
+                Some((opp, me)) => match opp {
+                    // rock
+                    "A" => match me {
+                        // lose
+                        "X" => scis + lose,
+                        // draw
+                        "Y" => rock + draw,
+                        // win
+                        "Z" => paper + win,
+                        _ => 0,
+                    },
+                    // paper
+                    "B" => match me {
+                        // lose
+                        "X" => rock + lose,
+                        // draw
+                        "Y" => paper + draw,
+                        // win
+                        "Z" => scis + win,
+                        _ => 0,
+                    },
+                    // scissors
+                    "C" => match me {
+                        // lose
+                        "X" => paper + lose,
+                        // draw
+                        "Y" => scis + draw,
+                        // win
+                        "Z" => rock + win,
+                        _ => 0,
+                    },
+                    _ => 0,
+                },
+                None => 0,
+            }
+        }
+    });
+    println!("part2: {}", score);
 }
