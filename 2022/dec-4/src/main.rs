43f3c395bbbdb79feec25e83110133e3e29af901
Brendan Armstrong <brendanarmstrong@proton.me>
2023-02-27 20:36:31 -0500

    got part one done

--- 2022/dec-4/src/main.rs
+++ 2022/dec-4/src/main.rs
@@ -1,3 +1,45 @@
+use std::{
+    fs,
+    io::{BufRead, BufReader},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let file = fs::File::open("input.txt").unwrap();
+    part1(file);
+}
+
+fn part1(f: fs::File) {
+    let reader = BufReader::new(f);
+
+    let answer: usize = reader
+        .lines()
+        .map(|v| {
+            let line = v.unwrap();
+            let Some((first, second)) = line.split_once(",") else {panic!("line wouldn't split")};
+            let Some((bf, ef)) = first.split_once("-") else {panic!("first didnt split")};
+            let Some((bs, es)) = second.split_once("-") else {panic!("second didnt split")};
+
+            let bf: usize = bf.parse().unwrap();
+            let ef: usize = ef.parse().unwrap();
+            let bs: usize = bs.parse().unwrap();
+            let es: usize = es.parse().unwrap();
+
+            if bf == bs || es == ef {
+                1
+            } else if bf > bs {
+                if ef > es {
+                    0
+                } else {
+                    1
+                }
+            } else {
+                if ef > es {
+                    1
+                } else {
+                    0
+                }
+            }
+        })
+        .sum();
+    println!("{}", answer);
 }
