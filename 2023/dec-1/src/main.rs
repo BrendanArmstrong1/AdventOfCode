ff9ec7af6c36572f1b37bcd27777cc716eed98c7
Brendan Armstrong <brendanarmstrong@proton.me>
2023-12-02 10:55:40 -0500

    finished part 2

--- 2023/dec-1/src/main.rs
+++ 2023/dec-1/src/main.rs
@@ -1,10 +1,12 @@
+use std::collections::HashMap;
 use std::fs;
 use std::io::{BufRead, BufReader};
 
 fn main() {
   let file = fs::File::open("input.txt").unwrap();
   part1(file);
-  // let file = fs::File::open("input.txt").unwrap();
+  let file = fs::File::open("input.txt").unwrap();
+  part2(file);
 }
 
 fn part1(f: fs::File) {
@@ -17,7 +19,50 @@ fn part1(f: fs::File) {
         numbers.push(char);
       }
     }
-    nums.push(format!("{}{}",numbers[0], numbers[numbers.len() - 1]).parse().unwrap())
+    nums.push(
+      format!("{}{}", numbers[0], numbers[numbers.len() - 1])
+        .parse()
+        .unwrap(),
+    );
   }
+  println!("{}", nums.iter().sum::<u32>());
+}
+
+fn part2(f: fs::File) {
+  let reader = BufReader::new(f);
+  let mut nums: Vec<u32> = Vec::new();
+  let numbers_map: HashMap<&str, char> = HashMap::from([
+    ("zero", '0'),
+    ("one", '1'),
+    ("two", '2'),
+    ("three", '3'),
+    ("four", '4'),
+    ("five", '5'),
+    ("six", '6'),
+    ("seven", '7'),
+    ("eight", '8'),
+    ("nine", '9'),
+  ]);
+  for line in reader.lines() {
+    let line = line.unwrap();
+    let mut found_text: Vec<(char, usize)> = Vec::new();
+    for (key, val) in numbers_map.iter() {
+      let pairs: Vec<_> = line.match_indices(key).collect();
+      for (idx, _) in pairs {
+        found_text.push((*val, idx));
+      }
+    }
+    for (idx, char) in line.chars().enumerate() {
+      if char.is_numeric() {
+        found_text.push((char, idx));
+      }
+    }
+    found_text.sort_by(|a, b| a.1.cmp(&b.1));
+    let value: u32 = format!("{}{}", found_text[0].0, found_text[found_text.len() - 1].0)
+      .parse()
+      .unwrap();
+    nums.push(value);
+  }
+
   println!("{}", nums.iter().sum::<u32>());
 }
