b48d4f6eb30fab04a02f484415a342d8a65e1bbc
Brendan Armstrong <brendanarmstrong@proton.me>
2024-12-02 11:37:43 -0500

    add a template C file

---
+++ 2024/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void part1(void);
+void part2(void);
+
+int main(void)
+{
+  printf("part 1 output:\n");
+  part1();
+  printf("part 2 output:\n");
+  part2();
+  return 0;
+}
