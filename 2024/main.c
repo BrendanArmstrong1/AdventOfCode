4d62858254f5673bdb3785e5d1c889017042319e
Brendan Armstrong <brendanarmstrong@proton.me>
2024-12-05 20:58:40 -0500

    adjusted the template file for more a better start

--- 2024/main.c
+++ 2024/main.c
@@ -1,45 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void test1(char *path);
-
-void part1(char *path);
-void part2(char *path);
-
 void test1(char *path)
 {
-  FILE *input;
+  FILE  *input;
+  char  *line = 0;
+  size_t n    = 0;
 
   input = fopen(path, "r");
-  if (!input) {
+  if (!input)
     exit(1);
-  }
+
+  while (getline(&line, &n, input) != -1)
+    printf("%s", line);
 }
 
-void part1(char *path)
+void part1(char *_)
 {
-  FILE *input;
-
-  input = fopen(path, "r");
-  if (!input) {
-    exit(1);
-  }
+  printf("Part 1\n");
 }
 
-void part2(char *path)
+void part2(char *_)
 {
-  FILE *input;
 
-  input = fopen(path, "r");
-  if (!input) {
-    exit(1);
-  }
+  printf("Part 2\n");
 }
 
 int main(void)
 {
   test1("./test.txt");
-  part1("./input.txt");
-  part2("./input.txt");
+  /*part1("./input.txt");*/
+  /*part2("./input.txt");*/
   return 0;
 }
